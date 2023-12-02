5dcc10e6672598c0f7b4d04a68313cac31c3cfeb
Patelz01 <151333252+Patelz01@users.noreply.github.com>
2023-12-02 00:31:20 -0800

    Add files via upload

---
+++ Assignment07.py
@@ -0,0 +1,314 @@
+# ------------------------------------------------------------------------------------------ #
+# Title: Assignment07
+# Desc: This assignment demonstrates using data classes
+# with structured error handling
+# Change Log: (Who, When, What)
+#   RRoot,1/1/2030,Created Script
+#   Zill Patel,11/29/23,Assignment7
+# ------------------------------------------------------------------------------------------ #
+import json
+
+# Define the Data Constants
+MENU: str = '''
+---- Course Registration Program ----
+  Select from the following menu:  
+    1. Register a Student for a Course.
+    2. Show current data.  
+    3. Save data to a file.
+    4. Exit the program.
+----------------------------------------- 
+'''
+FILE_NAME: str = "Enrollments.json"
+
+# Define the Data Variables
+students: list = []  # a table of student data
+menu_choice: str  # Hold the choice made by the user.
+
+
+class Person:
+    """
+    Create a Person class that has first name and last name.
+
+    ChangeLog: (Who, When, What)
+    Zill Patel, 12.1.2023, Created Class
+    """
+
+    def __init__(self, first_name: str = "", last_name: str = ""):
+        self.__first_name = first_name
+        self.__last_name = last_name
+
+    @property
+    def first_name(self):
+        return self.__first_name
+
+    @first_name.setter
+    def first_name(self, value: str):
+        if value.isalpha() or value == "":
+            self.__first_name = value
+        else:
+            raise ValueError("The first name should not contain numbers.")
+
+    @property
+    def last_name(self):
+        return self.__last_name
+
+    @last_name.setter
+    def last_name(self, value: str):
+        if value.isalpha() or value == "":
+            self.__last_name = value
+        else:
+            raise ValueError("The first name should not contain numbers.")
+
+    def __str__(self):
+        return f"{self.first_name},{self.last_name}"
+
+
+class Student(Person):
+    """
+    Create a student class that inherits from Person class and includes course name.
+
+    ChangeLog: (Who, When, What)
+    Zill Patel, 12.1.2023, Created Class
+    """
+
+    def __init__(self, first_name: str = "", last_name: str = "", course_name: str = ""):
+        super().__init__(first_name=first_name, last_name=last_name)
+        self.__course_name = course_name
+
+    @property
+    def course_name(self):
+        return self.__course_name
+
+    @course_name.setter
+    def course_name(self, value: str):
+        if value and value != "":
+            self.__course_name = value
+        else:
+            raise ValueError("Course name cannot be empty.")
+
+    def __str__(self):
+        return f"{self.first_name},{self.last_name},{self.course_name}"
+
+
+# Processing --------------------------------------- #
+class FileProcessor:
+    """
+    A collection of processing layer functions that work with Json files
+
+    ChangeLog: (Who, When, What)
+    Zill Patel, 12.1.2023, Updated functions to use Student class
+    """
+
+    @staticmethod
+    def read_data_from_file(file_name: str, student_data: list):
+        """
+        Function to read student data from file.
+
+        ChangeLog: (Who, When, What)
+        Zill Patel, 12.1.2023, Updated functions to use Student class
+
+        :param file_name: The name of the file to read from.
+        :param student_data: A list of Student objects.
+        :return: A list of Student objects.
+        """
+        try:
+            file = open(file_name, "r")
+
+            list_of_dictionary_data = json.load(file)  # the load function returns a list of dictionary rows.
+            for student in list_of_dictionary_data:
+                student_object = Student(first_name=student["FirstName"],
+                                                  last_name=student["LastName"],
+                                                  course_name=student["CourseName"])
+                student_data.append(student_object)
+            file.close()
+        except FileNotFoundError as e:
+            IO.output_error_messages("Text file must exist before running this script!", e)
+        except Exception as e:
+            IO.output_error_messages("There was a non-specific error!", e)
+        finally:
+            if file.closed == False:
+                file.close()
+        return student_data
+
+    @staticmethod
+    def write_data_to_file(file_name: str, student_data: list):
+        """
+        Function used to write student_data to file_name.
+
+        ChangeLog: (Who, When, What)
+        Zill Patel, 12.1.2023, Updated functions to use Student class
+
+        :param file_name: The name of file to write to.
+        :param student_data: A list of Student objects to write.
+        :return: None
+        """
+        try:
+            student_data_json = []
+            for student in student_data:
+                student_dict = {"FirstName": student.first_name,
+                                "LastName": student.last_name,
+                                "CourseName": student.course_name}
+                student_data_json.append(student_dict)
+
+            file = open(file_name, "w")
+            json.dump(student_data_json, file)
+            file.close()
+        except TypeError as e:
+            IO.output_error_messages("Please check that the data is a valid JSON format", e)
+        except Exception as e:
+            IO.output_error_messages("There was a non-specific error!", e)
+        finally:
+            if not file.closed:
+                file.close()
+
+
+# Presentation --------------------------------------- #
+class IO:
+    """
+    A collection of presentation layer functions that manage user input and output
+
+    ChangeLog: (Who, When, What)
+    Zill Patel, 12.1.2023, Updated functions to use Student class instead of student dictionary
+    ZPatel 12.1.23,Created Class
+    ZPatel 12.1.23,Added menu output and input functions
+    ZPatel 12.1.23,Added a function to display the data
+    ZPatel 12.1.23,Added a function to display custom error messages
+    """
+
+    @staticmethod
+    def output_error_messages(message: str, error: Exception = None):
+        """ This function displays a custom error messages to the user
+
+        ChangeLog: (Who, When, What)
+        ZPatel 11.30.23,Created function
+
+        :param message: string with message data to display
+        :param error: Exception object with technical message to display
+
+        :return: None
+        """
+        print(message, end="\n\n")
+        if error is not None:
+            print("-- Technical Error Message -- ")
+            print(error, error.__doc__, type(error), sep='\n')
+
+    @staticmethod
+    def output_menu(menu: str):
+        """ This function displays the menu of choices to the user
+
+        ChangeLog: (Who, When, What)
+        ZPatel 11.30.23,Created function
+
+
+        :return: None
+        """
+        print()  # Adding extra space to make it look nicer.
+        print(menu)
+        print()  # Adding extra space to make it look nicer.
+
+    @staticmethod
+    def input_menu_choice():
+        """ This function gets a menu choice from the user
+
+        ChangeLog: (Who, When, What)
+        ZPatel 11.30.23,Created function
+
+        :return: string with the users choice
+        """
+        choice = "0"
+        try:
+            choice = input("Enter your menu choice number: ")
+            if choice not in ("1", "2", "3", "4"):  # Note these are strings
+                raise Exception("Please, choose only 1, 2, 3, or 4")
+        except Exception as e:
+            IO.output_error_messages(e.__str__())  # Not passing e to avoid the technical message
+
+        return choice
+
+    @staticmethod
+    def output_student_and_course_names(student_data: list):
+        """ This function displays the student and course names to the user
+
+        ChangeLog: (Who, When, What)
+        Zill Patel, 12.1.2023, Updated functions to use Student class instead of student dictionary
+
+        :param student_data: list of dictionary rows to be displayed
+
+        :return: None
+        """
+
+        print("-" * 50)
+        for student in student_data:
+            print(f'Student {student.first_name} '
+                  f'{student.last_name} is enrolled in {student.course_name}')
+        print("-" * 50)
+
+    @staticmethod
+    def input_student_data(student_data: list):
+        """ This function gets the student's first name and last name, with a course name from the user
+
+        ChangeLog: (Who, When, What)
+        Zill Patel, 12.1.2023, Updated functions to use Student class instead of student dictionary
+
+        :param student_data: list of dictionary rows to be filled with input data
+
+        :return: list
+        """
+
+        try:
+            student = Student()
+            student.first_name = input("Enter the student's first name: ")
+            if not student.first_name.isalpha():
+                raise ValueError("The last name should not contain numbers.")
+
+            student.last_name = input("Enter the student's last name: ")
+            if not student.last_name.isalpha():
+                raise ValueError("The last name should not contain numbers.")
+            student.course_name = input("Please enter the name of the course: ")
+
+            student_data.append(student)
+            print()
+            print(f"You have registered {student.first_name} {student.last_name} for {student.course_name}.")
+        except ValueError as e:
+            IO.output_error_messages(message="One of the values was the correct type of data!", error=e)
+        except Exception as e:
+            IO.output_error_messages(message="Error: There was a problem with your entered data.", error=e)
+        return student_data
+
+
+# Start of main body
+
+# When the program starts, read the file data into a list of lists (table)
+# Extract the data from the file
+students = FileProcessor.read_data_from_file(file_name=FILE_NAME, student_data=students)
+
+# Present and Process the data
+while (True):
+
+    # Present the menu of choices
+    IO.output_menu(menu=MENU)
+
+    menu_choice = IO.input_menu_choice()
+
+    # Input user data
+    if menu_choice == "1":  # This will not work if it is an integer!
+        students = IO.input_student_data(student_data=students)
+        continue
+
+    # Present the current data
+    elif menu_choice == "2":
+        IO.output_student_and_course_names(students)
+        continue
+
+    # Save the data to a file
+    elif menu_choice == "3":
+        FileProcessor.write_data_to_file(file_name=FILE_NAME, student_data=students)
+        continue
+
+    # Stop the loop
+    elif menu_choice == "4":
+        break  # out of the loop
+    else:
+        print("Please only choose option 1, 2, or 3")
+
+print("Program Ended")
